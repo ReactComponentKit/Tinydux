7002a41ebbcd23ae119ba8d5fab55263c4fdf89c
skyfe79 <skyfe79@gmail.com>
2021-04-09 21:42:56 +0900

    generate project and setup example

---
+++ Example/TinyduxExample/TinyduxExample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TinyduxExample
+//
+//  Created by burt on 2021/04/09.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Example/TinyduxExample/TinyduxExample/TinyduxExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  TinyduxExampleApp.swift
+//  TinyduxExample
+//
+//  Created by burt on 2021/04/09.
+//
+
+import SwiftUI
+
+@main
+struct TinyduxExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
